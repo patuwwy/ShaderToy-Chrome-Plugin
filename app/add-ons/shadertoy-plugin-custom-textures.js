ddb78b97d187a006b8d8d4069a0786aaa8b0010f
Moritz Tim W <90388353+moritztim@users.noreply.github.com>
2025-06-04 00:55:50 -0300

    add file upload button

--- app/add-ons/shadertoy-plugin-custom-textures.js
+++ app/add-ons/shadertoy-plugin-custom-textures.js
@@ -31,6 +31,8 @@
 
     const S_RGB = false;
 
+    const ASSET_ID = 9;
+
     class MimeTypeError extends Error {
         /**
          * @param {string} adjective
@@ -101,4 +103,60 @@
         }
         throw new UnsupportedMimeTypeError(mimeType);
     }
+    /**
+     * Wraps the eventListener in a ridiculous construction to fit in with the other input options on the misc tab.
+     * @param {function} eventListener
+     */
+    function wrap(eventListener) {
+        const create = (...args) => document.createElement(...args);
+
+        const row = create('tr');
+        const td = create('td');
+
+        const divAsset = create('div');
+            divAsset.className = 'divAsset';
+            divAsset.id = 'miscAsset1';
+
+        const table = create('table');
+        const tbody = create('tbody');
+        const innerRow = create('tr');
+
+        const thumbnail = create('img');
+        thumbnail.className = 'inputThumbnail';
+        thumbnail.id = 'miscAssetThumnail1';
+        thumbnail.src = 'assets/file.png';
+
+        const triggerTd = create('td');
+
+        const infoTd = create('td');
+        infoTd.className = 'inputInfoColumn';
+
+        const infoDiv = create('div');
+        infoDiv.style.position = 'absolute';
+
+        infoDiv.innerHTML = `
+            <span class="spanName" id="miscAssetName${ASSET_ID}">File</span><br>
+            by <span class="spanUser" id="miscUserName${ASSET_ID}"><a class="user">you</a></span><br><br>
+            <span class="spanDescription" id="miscInfo${ASSET_ID}">upload your own file</span>
+        `;
+
+        row.append(td);
+        td.append(divAsset);
+        divAsset.append(table);
+        table.append(tbody);
+        tbody.append(innerRow);
+        innerRow.append(triggerTd, infoTd);
+        infoTd.append(infoDiv);
+        triggerTd.appendChild(thumbnail);
+        thumbnail.addEventListener('click', eventListener);
+
+        return row;
+    }
+
+    document.querySelector('#divMisc > table > tbody').appendChild(
+        wrap((event) => {
+            alert('Not Implemented');
+        })
+    );
+
 })();
